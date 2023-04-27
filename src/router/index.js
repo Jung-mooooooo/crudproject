b19708fde45d6b9925f9767061fc5716f1eeed1f
Jung-mooooooo <126451648+Jung-mooooooo@users.noreply.github.com>
2023-04-27 14:18:23 +0900

    2222

--- src/router/index.js
+++ src/router/index.js
@@ -1,105 +1,77 @@
 import { createRouter, createWebHistory } from "vue-router";
+
+import admin from './admin.js'
+import member from './member.js'
+
 import PageHome from "@/views/home/PageHome.vue";
-import BoardList from "@/views/board/BoardList.vue";
-import BoardDetail from "@/views/board/BoardDetail.vue";
-import BoardWrite from "@/views/board/BoardWrite.vue";
+import PageHomeLogin from "@/views/home/PageHomeLogin.vue";
+
+// import BoardList from "@/views/board/BoardList.vue";
+// import BoardDetail from "@/views/board/BoardDetail.vue";
+// import BoardWrite from "@/views/board/BoardWrite.vue";
+
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const routes = [
   {
-    path: "/",
-    name: "PageHome",
-    component: PageHome,
+    path: '/',
+    name: 'defaultLayout',
+    component: () => import('../components/layouts/DefaultLayout.vue'),
+    children: [
+      {
+        path: "/",
+        name: "PageHome",
+        component: PageHome,
+      },
+      {
+        path: "/about",
+        name: "About",
+        component: () => import("../views/home/PageAbout.vue"),
+      },
+      {
+        path: "/login",
+        name: "PageHomeLogin",
+        component: PageHomeLogin,
+      },
+]
+
   },
   {
-    path: "/admin",
-    name: "Admin",
-    component: () => import("../views/admin/AdminHome.vue"),
+    path: '/',
+    name: 'NoNavLayout',
+    component: () => import('../components/layouts/NoNavLayout.vue'),
+    children: [
+      {
+        path: '/special/',
+        name: 'SpecialHome',
+        component: () => import('../views/special/SpecialHome.vue'),
+      },
+      {
+        path: '/special/speech',
+        name: 'SpecialSpeech',
+        component: () => import('../views/special/SpecialSpeech.vue'),
+      },
+      {
+        path: '/special/music',
+        name: 'SpecialMusic',
+        component: () => import('../views/special/SpecialMusic.vue'),
+      },
+    ]
   },
-  {
-    path: "/admin/counselling",
-    name: "Counselling",
-    component: () => import("../views/admin/AdminCounselling.vue"),
-  },
-  {
-    path: "/admin/welfareProgram",
-    name: "welfareProgram",
-    component: () => import("../views/admin/AdminWelfareProgram.vue"),
-  },
-  {
-    path: "/admin/counsellingCenter",
-    name: "counsellingCenter",
-    component: () => import("../views/admin/AdminCounsellingCenter.vue"),
-  },
-  {
-    path: "/admin/speechRecognitionHelper",
-    name: "speechRecognitionHelper",
-    component: () => import("../views/admin/AdminSpeechRecognitionHelper.vue"),
-  },
-  {
-    path: "/admin/AdminWelfareFacility",
-    name: "AdminWelfareFacility",
-    component: () => import("../views/admin/AdminWelfareFacility.vue"),
-  },
-  {
-    path: "/admin/AdminTouristSpot",
-    name: "AdminTouristSpot",
-    component: () => import("../views/admin/AdminTouristSpot.vue"),
-  },
-  {
-    path: "/admin/AdminHobby",
-    name: "AdminHobby",
-    component: () => import("../views/admin/AdminHobby.vue"),
-  },
-  {
-    path: "/welfareProgram/welfareFacility",
-    name: "WelfareFacility",
-    component: () => import("../views/welfareProgram/WelfareFacility.vue"),
-  },
-  {
-    path: "/welfareProgram/touristSpot",
-    name: "TouristSpot",
-    component: () => import("../views/welfareProgram/TouristSpot.vue"),
-  },
-  {
-    path: "/welfareProgram/hobby",
-    name: "HobbyMain",
-    component: () => import("../views/welfareProgram/HobbyMain.vue"),
-  },
-  {
-    path: "/welfareProgram/welfareFacilityMap",
-    name: "WelfareFacilityMap",
-    component: () => import("../views/welfareProgram/WelfareFacilityMap.vue"),
-  },
-  {
-    path: "/welfareProgram/touristSpotMap",
-    name: "TouristSpotMap",
-    component: () => import("../views/welfareProgram/TouristSpotMap.vue"),
-  },
-  {
-    path: "/welfareProgram/hobbyMap",
-    name: "hobbyMap",
-    component: () => import("../views/welfareProgram/HobbyMap.vue"),
-  },
-  {
-    path: "/board/list",
-    name: "BoardList",
-    component: BoardList,
-  },
-  {
-    path: "/board/detail",
-    name: "BoardDetail",
-    component: BoardDetail,
-  },
-  {
-    path: "/board/write",
-    name: "BoardWrite",
-    component: BoardWrite,
-  },
+
+
 ];
+
+const allrouter = [
+  routes,
+  ...admin,
+  ...member
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
+  routes: allrouter
+})
 
 export default router;
