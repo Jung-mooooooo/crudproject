87e4ed2eb1aed3a3df6c6fbafb1061d201a4cec2
Jung-mooooooo <126451648+Jung-mooooooo@users.noreply.github.com>
2023-04-27 12:37:26 +0900

    asdf

---
+++ src/router/member.js
@@ -0,0 +1,74 @@
+// import { createRouter, createWebHistory } from "vue-router";
+import MemberLogin from "@/views/member/MemberLogin.vue";
+import MemberEnroll from "@/views/member/MemberEnroll.vue";
+import MemberFindId from "@/views/member/MemberFindId.vue";
+import MemberFindPw from "@/views/member/MemberFindPw.vue";
+import MemberMyinfo from "@/views/member/MemberMyinfo.vue";
+import MemberUpdate from "@/views/member/MemberUpdate.vue";
+import Mypage from "@/views/member/Mypage.vue";
+import PopUpdate from "@/views/member/popupToUpdate.vue";
+import PopDelete from "@/views/member/popupToDelete.vue";
+
+
+const member = [{
+
+    path: '/',
+    name: 'defaultLayout',
+    component: () => import('../components/layouts/DefaultLayout.vue'),
+    children: [
+        {
+            path: "/member/login",
+            name: "MemberLogin",
+            component: MemberLogin,
+        },
+        {
+            path: "/member/enroll",
+            name: "MemberEnroll",
+            component: MemberEnroll,
+        },
+        {
+            path: "/member/findid",
+            name: "MemberFindId",
+            component: MemberFindId,
+        },
+        {
+            path: "/member/findpw",
+            name: "MemberFindPw",
+            component: MemberFindPw,
+        },
+        {
+            path: "/member/myinfo",
+            name: "MemberMyinfo",
+            component: MemberMyinfo,
+        },
+        {
+            path: "/member/update",
+            name: "MemberUpdate",
+            component: MemberUpdate,
+        },
+        {
+            path: "/mypage",
+            name: "Mypage",
+            component: Mypage,
+        },
+        {
+            path: "/mypage/popupU",
+            name: "PopUpdate",
+            component: PopUpdate,
+        },
+        {
+            path: "/mypage/popupD",
+            name: "PopDelete",
+            component: PopDelete,
+        },
+
+    ]
+}
+]
+
+//   const router = createRouter({
+//     history: createWebHistory(process.env.BASE_URL),
+//     routes,
+//   });
+
+export default member;
