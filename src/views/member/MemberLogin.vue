3eb4e467718accf4c043b6479b43e739d7d8a49a
Jung-mooooooo <126451648+Jung-mooooooo@users.noreply.github.com>
2023-05-26 15:58:14 +0900

    Update MemberLogin.vue

--- src/views/member/MemberLogin.vue
+++ src/views/member/MemberLogin.vue
@@ -197,7 +197,7 @@ export default {
           headers: {
             'Content-Type': 'application/json',
             Authorization: 'token',
-          }
+          },
         }
         )
         .then((res) => {
